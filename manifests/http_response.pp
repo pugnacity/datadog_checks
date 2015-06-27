fcc812dcbe8d703269e2f27534588f11fb09e5c1
Sven Winkelmann <s.winkelmann@pinguin.ag>
2015-06-27 15:58:54 +0200

    changed documentation

--- manifests/http_response.pp
+++ manifests/http_response.pp
@@ -1,21 +1,13 @@
 # == Class: datadog_checks::http_response
 #
 #
-#
 # === Parameters
 #
 # this class takes no parameters
 #
 # === Variables
 #
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# this class has no Variables
 #
 # === Examples
 #
